e3a6c4ce8d71b0ba9509d18256d2d2c5788b82a4
Bari <brownjab21@gmail.com>
2023-02-23 02:50:30 -0600

    feat: transaction sending methods

--- fiber/client.py
+++ fiber/client.py
@@ -62,3 +62,23 @@ class Client:
 
     def subscribe_new_blocks(self):
         return self.stub.SubscribeNewBlocks(api_pb2.google_dot_protobuf_dot_empty__pb2.Empty(), metadata=self.metadata)
+    
+    def send_transaction(self, tx: Transaction):
+        res = self.stub.SendTransaction(tx, metadata=self.metadata)
+        
+        return res.hash, res.timestamp
+
+    def send_transaction_sequence(self, txs: list[Transaction]):
+        res = self.stub.SendTransactionSequence(txs, metadata=self.metadata)
+        
+        return map(lambda sequence_response: sequence_response.hash, res), res[0].timestamp
+
+    def send_raw_transaction(self, tx: bytes):
+        res = self.stub.SendRawTransaction(tx, metadata=self.metadata)
+        
+        return res.hash, res.timestamp
+
+    def send_raw_transaction_sequence(self, txs: list[bytes]):
+        res = self.stub.SendRawTransactionSequence(txs, metadata=self.metadata)
+        
+        return map(lambda sequence_response: sequence_response.hash, res), res[0].timestamp 
